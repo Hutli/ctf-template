90a5894115f08192f3197d1cef01bf3461112e2d
Alexander Krog <moonfrogdk@gmail.com>
2020-07-28 13:56:07 +0200

    update

--- setup.py
+++ setup.py
@@ -9,18 +9,21 @@ import subprocess
 from time import sleep
 import atexit
 import re
+from subprocess import check_output
 
 #settings
-terminalSetting = ['urxvt', '-e', 'zsh', '-c']
+#terminalSetting = ['termite', '-e']
+terminalSetting = ['tmux', 'new-window']
 dockerSettings = ['tmux', 'new-window']
 systemLd = '/usr/lib/ld-linux-x86-64.so.2'
 systemLibc = '/usr/lib/libc.so.6'
 dockerLd = '/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2'
 dockerLibc = '/lib/x86_64-linux-gnu/libc.so.6'
+dockerRunPath = '/root/'
 pwndbgSettings = '''
-set context-code-lines 8
-set context-stack-lines 4
-set context-sections "regs disasm code stack"
+set context-code-lines 10
+set context-stack-lines 8
+set context-sections "regs disasm code stack backtrace"
 '''
 
 gefSettings = '''
@@ -60,8 +63,12 @@ parser.add_argument('--pre-load-extras', dest='preloadExtras', default=None,
                     help='extra files to preload')
 parser.add_argument('--libraries', dest='libraries', default=None,
                     help='sets the LD_LIBRARY_PATH (default is None)')
+parser.add_argument('--extra-src-dir', dest='srcDirs', default=[],
+                    help='extra dirs to search for src files')
 parser.add_argument('--exec', dest='exec', default='attach',
                     help='how to execute debug/local/remote/attach')
+parser.add_argument('--exe', dest='exe', default=None,
+                    help='the elf passed to gdb')
 parser.add_argument('--host', dest='host', default='None',
                     help='ip of remote host')
 parser.add_argument('--port', dest='port', default=None,
@@ -84,10 +91,17 @@ parser.add_argument("--inside-docker", dest='insideDocker', type=str2bool, nargs
 parser.add_argument("--docker", dest='docker', type=str2bool, nargs='?',
                         const=True, default=False,
                         help="run inside docker")
+parser.add_argument('--extra-docer-param', dest='dockerParam', default='',
+                    help='extra docker parameters')
+parser.add_argument('--attach-to', dest='attachTo', default='',
+                    help='extra docker parameters')
 parser.add_argument('onegadget', metavar='N', nargs='*',
                     help='onegadget to try')
 
 args = parser.parse_args()
+
+def get_pid(name):
+    return min([int(x) for x in check_output(["pidof",name]).split()])
 
 def parseBreakPoints(breakpoints: str, path: str, pid: int = None):
     if not pid:
@@ -106,7 +120,11 @@ def parseBreakPoints(breakpoints: str, path: str, pid: int = None):
             'permission': l[1], 'offset': l[2], 'device': l[3], 'indode': l[4],
             'path': l[5] if len(l) == 6 else 'data'}
             objs.append(obj)
-        mainObj = sorted([x for x in objs if x['path'] == path], key=lambda x: x['start'])[0]
+        mainObj = sorted([x for x in objs if x['path'] == path], key=lambda x: x['start'])
+        if mainObj:
+            mainObj = mainObj[0]
+        else:
+            mainObj = None
         newBreakpoints = []
         for l in breakpoints.split('\n'):
             if l:
@@ -118,13 +136,20 @@ def parseBreakPoints(breakpoints: str, path: str, pid: int = None):
                         val = obj['start'] + val
                         newBreakpoints.append('b *' + hex(val))
                     else:
-                        val = int(l.split('pie ')[1], 16)
-                        val = mainObj['start'] + val
-                        newBreakpoints.append('b *' + hex(val))
+                        if mainObj:
+                            val = int(l.split('pie ')[1], 16)
+                            val = mainObj['start'] + val
+                            newBreakpoints.append('b *' + hex(val))
                 else:
                     newBreakpoints.append(l)
         newBreakpoints = '\n'.join(newBreakpoints)
         return newBreakpoints
+
+def fast_scandir(dirname):
+    subfolders= [f.path for f in os.scandir(dirname) if f.is_dir()]
+    for dirname in list(subfolders):
+        subfolders.extend(fast_scandir(dirname))
+    return subfolders
 
 def setup(elfPath: str, breakpoints: str, progArgs: List[str] = None, extraGdbSettings: str = ''):
     gdbSettings = '''set breakpoint pending on'''
@@ -166,7 +191,7 @@ def setup(elfPath: str, breakpoints: str, progArgs: List[str] = None, extraGdbSe
         p = subprocess.run('docker image ls'.split(), capture_output=True)
         out = p.stdout.decode('utf-8')
         if containerName not in out:
-            buildCmd = f'docker build -t {containerName} .'
+            buildCmd = 'docker build -t ' + containerName + ' .'
             p = subprocess.run(buildCmd.split(), capture_output=True)
             if p.stderr:
                 log.success('docker build failed')
@@ -176,32 +201,35 @@ def setup(elfPath: str, breakpoints: str, progArgs: List[str] = None, extraGdbSe
         else:
             log.info('docker already exists')
 
-        kill = f'docker kill {containerName}'
+        kill = 'docker kill ' + containerName
         p = subprocess.run(kill.split(), capture_output=True)
         if p.stdout:
             log.info('killed old container')
 
-        runCmd = f'docker run -d --name {containerName} --rm -it {containerName}'
+        runCmd = 'docker run -p 1024:1024' + args.dockerParam + ' --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -d --name ' + containerName + ' --rm -it ' + containerName
         p = subprocess.run(runCmd.split(), capture_output=True)
         if p.stdout:
             log.success('docker container running')
 
-        newSesCmd = f'docker exec -it {containerName} tmux new-session -d -s debug'
+        newSesCmd = 'docker exec -it ' + containerName + ' tmux new-session -d -s debug'
         p = subprocess.run(newSesCmd.split(), capture_output=True)
         with open('.debugDocker.sh', 'w') as f:
             f.write('#!/bin/bash\n')
-            f.write(f'tmux send-keys -t debug.0 "{cmd}" ENTER\n')
+            f.write('tmux send-keys -t debug.0 "' + cmd + '" ENTER\n')
         
-        p = subprocess.run(f'docker cp setup.py {containerName}:/root/setup.py'.split(), capture_output=True)
-        p = subprocess.run(f'docker cp exploit.py {containerName}:/root/exploit.py'.split(), capture_output=True)
-        p = subprocess.run(f'docker cp .debugDocker.sh {containerName}:/root/.debugDocker.sh'.split(), capture_output=True)
-        p = subprocess.run(f'docker exec -it {containerName} chmod +x /root/.debugDocker.sh'.split(), capture_output=True)
-        p = subprocess.run(f'docker exec -it {containerName} sh /root/.debugDocker.sh'.split(), capture_output=True)
+        p = subprocess.run(('docker cp setup.py ' + containerName + ':' + dockerRunPath + '/setup.py').split(), capture_output=True)
+        p = subprocess.run(('docker cp exploit.py ' + containerName + ':' + dockerRunPath + '/exploit.py').split(), capture_output=True)
+        p = subprocess.run(('docker cp .tmux.conf ' + containerName + ':/root/.tmux.conf').split(), capture_output=True)
+        p = subprocess.run(('docker cp setupSplit.py ' + containerName + ':' + dockerRunPath + '/setupSplit.py').split(), capture_output=True)
+        p = subprocess.run(('docker cp .debugDocker.sh ' + containerName + ':' + dockerRunPath + '/.debugDocker.sh').split(), capture_output=True)
+        p = subprocess.run(('docker exec -it ' + containerName + ' chmod +x ' + dockerRunPath + '/.debugDocker.sh').split(), capture_output=True)
+        p = subprocess.run(('docker exec -it ' + containerName + ' sh ' + dockerRunPath + '/.debugDocker.sh').split(), capture_output=True)
+        p = subprocess.run(('docker cp httpd.conf ' + containerName + ':/usr/local/apache2/conf/httpd.conf').split(), capture_output=True)
 
-        cmd = f'docker exec -it {containerName} tmux a -t debug'
+        cmd = 'docker exec -it ' + containerName + ' tmux a -t debug'
 
         os.system(cmd)
-
+        print(containerName)
         quit()
 
     if args.patch:
@@ -231,7 +259,7 @@ def setup(elfPath: str, breakpoints: str, progArgs: List[str] = None, extraGdbSe
     if args.preloadExtras:
         preloadString += ' ' + args.preloadExtras
 
-    env = my_env = os.environ.copy()
+    env = os.environ.copy()
     
     if preloadString != '':
         env["LD_PRELOAD"] = preloadString
@@ -260,6 +288,40 @@ def setup(elfPath: str, breakpoints: str, progArgs: List[str] = None, extraGdbSe
 
     pid = None
 
+    if args.insideDocker:
+        runPath = dockerRunPath
+    else:
+        runPath = './'
+    runPath = os.path.abspath(runPath)
+    
+    glibcPath = runPath + '/glibc'
+    if os.path.isdir(glibcPath):
+        dirs = fast_scandir(glibcPath)
+        dirs = ['dir ' + x for x in dirs]
+        dirs = '\n'.join(dirs)
+        dirs += '\ndir ./'
+    else:
+        dirs = '\ndir ./'
+    extraGdbSettings += dirs
+    
+    if args.srcDirs and os.path.isdir(args.srcDirs):
+        dirs = fast_scandir(args.srcDirs)
+        dirs = ['dir ' + x for x in dirs]
+        dirs = '\n'.join(dirs)
+        extraGdbSettings += dirs    
+    
+    if args.exec == 'scriptRemote':
+        io = process(args.program.split())
+        #sleep(5)
+        gdbSettings += parseBreakPoints(breakpoints, elfPath, pid) + extraGdbSettings
+        host, port = args.host, int(args.port)
+        pwnlib.gdb.attach((host, port), gdbSettings, exe = elfPath if not args.exe else args.exe)
+    if args.exec == 'script':
+        p = subprocess.run(args.program.split())
+        pid = get_pid(args.attachTo)
+        gdbSettings += parseBreakPoints(breakpoints, elfPath, pid) + extraGdbSettings
+        io = None
+        pwnlib.gdb.attach(pid, gdbSettings)
     if args.exec == 'attach':
         io = process(prog, env=env)
         pid = io.pid
@@ -281,7 +343,7 @@ def setup(elfPath: str, breakpoints: str, progArgs: List[str] = None, extraGdbSe
         libsoPath = env["LD_LIBRARY_PATH"] + '/libc.so.6'
         if os.path.exists(libsoPath):
             os.remove(libsoPath)
-        cmd = f'ln -s {args.libc} {libsoPath}'
+        cmd = 'ln -s ' + args.libc + ' ' + libsoPath
         log.info('creating libc.so.6: ' + cmd)
         cmd = cmd.split()
         p = subprocess.run(cmd, capture_output=True)
@@ -289,7 +351,6 @@ def setup(elfPath: str, breakpoints: str, progArgs: List[str] = None, extraGdbSe
         if args.exec == 'ldAttach':
             io = process(prog, env=env)
             pid = io.pid
-            sleep(1)
             gdbSettings += parseBreakPoints(breakpoints, elfPath, pid) + '\n' + extraGdbSettings
             pwnlib.gdb.attach(io, gdbSettings)
         if args.exec == 'ldDebug':
