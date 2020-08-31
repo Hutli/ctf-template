905818d3aef5181c39efba368b65aaa88780bc68
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2020-08-31 14:16:04 +0200

    Backup

--- attach.sh
+++ attach.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python exploit.py --gdbplugin pwndbg --ld ld-2.27.so --libc libc-2.27.so --pre-load-libc --auto-patch --exec attach --program ropmev2
+python exploit.py --gdbplugin pwndbg --exec attach --program sh
--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python exploit.py --gdbplugin pwndbg --ld someld.so --libc somelibc.so --exec debug --program someProgram
+python exploit.py --gdbplugin pwndbg --exec debug --program sh
